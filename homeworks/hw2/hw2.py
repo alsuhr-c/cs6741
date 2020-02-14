Fixed off-by-one error.

homeworks/hw2/hw2.py
@@ -9,8 +9,8 @@ from torchtext.vocab import Vectors
 TEXT = torchtext.data.Field()
 
 train, val, test = torchtext.datasets.LanguageModelingDataset.splits(
-#    path='/home/alsuhr/Documents/cs6741/homeworks/hw2/ptb/', 
-    path='/Users/alsuhr/Documents/Cornell/cs6741/ptb/',
+    path='/home/alsuhr/Documents/cs6741/homeworks/hw2/ptb/', 
+#    path='/Users/alsuhr/Documents/Cornell/cs6741/ptb/',
     train='train.txt', validation='valid.txt', test='test.txt',
     text_field=TEXT)
 
@@ -28,11 +28,11 @@ print('Loaded %s val examples' % len(val))
 print('Loaded %s testing examples' % len(test))
 print('Vocab size is %s' % len(TEXT.vocab))
 
-train_iter, val_iter, test_iter = BPTTIterator.splits((train, val, test), batch_size=10,
-#                                                      device=torch.device('cuda'),
+train_iter, val_iter, test_iter = BPTTIterator.splits((train, val, test), batch_size=512,
+                                                      device=torch.device('cuda'),
                                                       bptt_len=32, repeat=False)
 
 if '--count_based_model' in sys.argv:
     train_count_based_model(train_iter, val_iter, test_iter, TEXT, 0.3, 0.7)
 elif '--feedforward_model' in sys.argv:
-    train_feedforward_language_model(train_iter, val_iter, test_iter, TEXT, 3, 64)
+    train_feedforward_language_model(train_iter, val_iter, test_iter, TEXT, 8, 512)

homeworks/hw2/util.py
@@ -6,9 +6,9 @@ EOS = '<eos>'
 UNK = '<unk>'
 
 if torch.cuda.device_count() >= 1:
-    DEVICE: torch.device = torch.device('cuda')
+    DEVICE = torch.device('cuda')
 else:
-    DEVICE: torch.device = torch.device('cpu')
+    DEVICE = torch.device('cpu')
 
 
 def evaluate_perplexity(model, data_iter, logits=False):
@@ -28,7 +28,7 @@ def evaluate_perplexity(model, data_iter, logits=False):
             seq_length, batch_size, vocab_size = probabilities.size()
 
             flattened_probabilities = probabilities.view(seq_length * batch_size, -1)
-            flattened_targets = batch.target.view(seq_length * batch_size)
+            flattened_targets = batch.text.view(seq_length * batch_size)
 
             prob_list = list()
             for i in range(seq_length * batch_size):
@@ -57,7 +57,7 @@ def train_model(model, train_iter, val_iter, optimizer, loss_fn):
             for batch in train_it:
                 optimizer.zero_grad()
                 logits = model(batch)
-                labels = batch.target
+                labels = batch.text
 
                 timestep, batch_size, vocab_size = logits.size()
 
@@ -71,9 +71,10 @@ def train_model(model, train_iter, val_iter, optimizer, loss_fn):
                 pbar.update(1)
 
         model.eval()
-        train_ppl = evaluate_perplexity(model, train_iter, True)
-        best = False
-        val_ppl = evaluate_perplexity(model, val_iter, True)
+        with torch.no_grad():
+            train_ppl = evaluate_perplexity(model, train_iter, True)
+            best = False
+            val_ppl = evaluate_perplexity(model, val_iter, True)
         if val_ppl < min_ppl:
             torch.save(model.state_dict(), 'best_model.pt')
             min_ppl = val_ppl